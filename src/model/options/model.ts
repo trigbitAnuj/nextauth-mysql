00401f1af2550a30079e13fcc46f08248cbfdc87
Anuj kumar <trigbit.anuj@gmail.com>
2023-08-17 18:41:09 +0530

    Create model.ts

---
+++ src/model/options/model.ts
@@ -0,0 +1,45 @@
+import {
+  BelongsTo,
+  Column,
+  CreatedAt,
+  DataType,
+  ForeignKey,
+  Model,
+  Table,
+  UpdatedAt,
+} from "sequelize-typescript";
+import { IOptions, OptionsCreationAttribute } from "./interface";
+import Question from "../question/model";
+
+@Table({ tableName: "Options", timestamps: true })
+class Option
+  extends Model<Option, OptionsCreationAttribute>
+  implements IOptions
+{
+  @Column({
+    type: DataType.UUID,
+    defaultValue: DataType.UUIDV1,
+    primaryKey: true,
+  })
+  public declare id: string;
+
+  @ForeignKey(() => Question)
+  @Column(DataType.UUID)
+  public questionId!: string;
+
+  @BelongsTo(() => Question, {
+    foreignKey: "questionId",
+    as: "question",
+  })
+  public question!: ReturnType<() => Question>;
+
+  @Column({ type: DataType.STRING })
+  option!: string;
+
+  @CreatedAt
+  public declare readonly createdAt: Date;
+
+  @UpdatedAt
+  public declare readonly updatedAt: Date;
+}
+export default Option;
