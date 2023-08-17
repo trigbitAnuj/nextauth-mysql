8b4fb132d902a04949cab2bc1bae2bdc938e0e5f
Anuj kumar <trigbit.anuj@gmail.com>
2023-08-17 18:43:48 +0530

    Update route.ts

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,17 +1,19 @@
 import { genSalt, hash } from "bcryptjs";
 import { NextRequest, NextResponse } from "next/server";
-import { User } from "@/model/userModel";
+import { models } from "@/config/mysql";
+import { Role } from "@/model/user/interface";
 
 export const POST = async (req: NextRequest) => {
   try {
     const body = await req.json();
-    const { username, email, password } = body as {
+    const { username, email, password, role } = body as {
       username: string;
       email: string;
       password: string;
+      role: Role;
     };
 
-    if (!email || !password) {
+    if (!email || !password || !role || !username) {
       return NextResponse.json(
         { message: "Email and password are required" },
         { status: 400 }
@@ -20,22 +22,23 @@ export const POST = async (req: NextRequest) => {
 
     // check if user exists
 
-    const user = await User.findOne({ where: { email } });
+    // const user = await models.User.findOne({ where: { email } });
 
-    if (user) {
-      return NextResponse.json(
-        { message: "User already exists" },
-        { status: 409 }
-      );
-    }
+    // if (user) {
+    //   return NextResponse.json(
+    //     { message: "User already exists" },
+    //     { status: 409 }
+    //   );
+    // }
 
     const salt = await genSalt(12);
     const hashedPassword = await hash(password, salt);
 
-    const newUser = await User.create({
+    const newUser = await models.User.create({
       name: username,
       email,
       password: hashedPassword,
+      role,
     });
     const savedUser = await newUser.save();
     console.log(savedUser, "line 65");
