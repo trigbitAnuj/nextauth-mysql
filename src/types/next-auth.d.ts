10ce7f1d921c497959a3ac1148cbcc251441cce3
Anuj kumar <trigbit.anuj@gmail.com>
2023-08-17 18:40:14 +0530

    Update next-auth.d.ts

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,7 +1,16 @@
+import { UserRole } from "@/model/userModel";
 import { User } from "next-auth";
+
+type UserId = string;
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: UserId;
+    role: UserRole;
+  }
+}
 
 declare module "next-auth" {
   interface Session {
-    user: User & { id: string };
+    user: User & { id: UserId; role: UserRole };
   }
 }
