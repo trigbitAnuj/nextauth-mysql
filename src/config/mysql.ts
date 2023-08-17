238309e136d95ac443cd468fdc0ac0807edaa776
Anuj kumar <trigbit.anuj@gmail.com>
2023-08-17 18:42:15 +0530

    Update mysql.ts

--- src/config/mysql.ts
+++ src/config/mysql.ts
@@ -1,20 +1,38 @@
 import mysql2 from "mysql2";
-import { Sequelize } from "sequelize";
+import { Sequelize } from "sequelize-typescript";
 import config from "../../config/config.json";
+import Category from "@/model/category/model";
+import User from "@/model/user/model";
+import Question from "@/model/question/model";
+import Option from "@/model/options/model";
+
+const models = {
+  Category,
+  User,
+  Question,
+  Option,
+};
 
 const dbConfig = config[process.env.NODE_ENV];
 const sequelize = new Sequelize({
   ...(dbConfig as any),
   dialectModule: mysql2,
   port: 3306,
+  models: Object.values(models),
 });
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Connection has been established successfully.");
-  })
-  .catch((error: Error) => {
-    console.error("Unable to connect to the database:", error);
-  });
-export { sequelize };
+const initDb = async () => {
+  try {
+    if (!(global as any).dbInit) {
+      await sequelize.authenticate();
+      console.log("db created successfully");
+      await sequelize.sync({ alter: false, hooks: true });
+      (global as any).dbInit = true;
+    }
+  } catch (error) {
+    console.log("db init failed");
+    throw error;
+  }
+};
+
+export { sequelize, models, initDb };
