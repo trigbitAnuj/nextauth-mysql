f95f58427e9fcabab3e8e6c2398def4fceab36c2
Anuj kumar <trigbit.anuj@gmail.com>
2023-08-18 18:59:05 +0530

    Update category.ts

--- src/graphql/schema/category.ts
+++ src/graphql/schema/category.ts
@@ -1,6 +1,17 @@
 import { gql } from "@apollo/client";
 
 module.exports = gql`
+  type Category {
+    id: String
+    name: String
+    questions: [Questions!]
+  }
+
+  type Query {
+    getCategories: [Category!]
+    getSingleCategory(categoryId: String!): Category
+  }
+
   type Mutation {
     createCategory(name: String!): CreateCategoryResponse
   }
