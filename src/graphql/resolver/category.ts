3c8161affbe601f0e098bead2caabaa0a7a35998
Anuj kumar <trigbit.anuj@gmail.com>
2023-08-18 18:59:33 +0530

    Update category.ts

--- src/graphql/resolver/category.ts
+++ src/graphql/resolver/category.ts
@@ -1,15 +1,40 @@
 import { models } from "@/config/mysql";
+import Category from "@/model/category/model";
+
+type ParentId = {
+  id: string;
+};
 
 module.exports = {
-  Mutation: {
-    async createCategory(
-      parent: any,
-      { name }: { name: string },
-      contextValue: any,
-      info: any
-    ) {
-      console.log(parent, name, contextValue, info);
+  Query: {
+    getCategories: async () => {
+      const allCategories = await models.Category.findAll();
+      return allCategories;
+    },
 
+    getSingleCategory: async (
+      _: any,
+      { categoryId }: { categoryId: string }
+    ) => {
+      console.log(categoryId);
+      const singleCategory = await models.Category.findByPk(categoryId);
+      console.log(singleCategory);
+      return singleCategory;
+    },
+  },
+
+  Category: {
+    questions: async (root: any) => {
+      console.log(root);
+      const question = root.getQuestions();
+      console.log(question);
+      return root.getQuestions();
+    },
+  },
+
+  Mutation: {
+    createCategory: async (parent: any, { name }: { name: string }) => {
+      console.log(parent, name);
       return await models.Category.create({
         name,
       });
