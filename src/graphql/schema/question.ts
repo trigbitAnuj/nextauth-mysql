dcfce7aa5bc27050f26ac816b8f4c00e830653e8
Anuj kumar <trigbit.anuj@gmail.com>
2023-08-18 18:59:01 +0530

    Update question.ts

--- src/graphql/schema/question.ts
+++ src/graphql/schema/question.ts
@@ -1,6 +1,16 @@
 import { gql } from "@apollo/client";
 
 module.exports = gql`
+  type Questions {
+    id: String
+    name: String
+    categoryId: String
+  }
+
+  type Query {
+    getQuestionsWithId(categoryId: String): Questions
+  }
+
   type Mutation {
     createQuestion(text: String!): CreateQuestionResponse
   }
